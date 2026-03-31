8429d3246442bf216c32a38abf624db885b5b798
Grayson Hamp <grayson.champ3@gmail.com>
2026-03-30 23:05:42 -0600

    Started Inventory

---
+++ Scripts/Items/Item.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Item : Area2D
+{
+	[Export] public string ItemName = "";
+	[Export] public Texture2D icon;
+	[Export] public bool IsStackable = false;
+
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		AddToGroup("items");
+	}
+
+}
