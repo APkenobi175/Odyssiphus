458a6d8138eb66b988e9907aea790efe59ed0de3
Ryan V <a02304400@usu.edu>
2026-04-21 13:06:13 -0600

    dividing slime enemies

---
+++ Scripts/Entities/Slime.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class Slime : Entity, IProjectile
+{
+  [Export]
+  public float SpawnDistance = 50;
+  public void Initialize(Vector2 position, Vector2 direction)
+  {
+    Position = position + direction.Normalized() * SpawnDistance;
+    GD.Print($"New slime spawned at {Position}, {GlobalPosition}");
+  }
+}
